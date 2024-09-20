b65c4cbb729bebd482a518aa97101bbf5bb9f4c2
vvisun <374708234@qq.com>
2024-09-20 22:00:55 +0800

    event添加注销监听接口

--- event/event.go
+++ event/event.go
@@ -22,14 +22,22 @@ func EventInstance() *eventBusWrapper {
 	return ebr
 }
 
+// 监听topic事件,同步
 func (e *eventBusWrapper) Subscribe(topic string, fn interface{}) error {
 	return e.eb.Subscribe(topic, fn)
 }
 
+// 监听topic事件,异步
 func (e *eventBusWrapper) SubscribeAsync(topic string, fn interface{}, transactional bool) error {
 	return e.eb.SubscribeAsync(topic, fn, transactional)
 }
 
+// 发布topic事件
 func (e *eventBusWrapper) Publish(topic string, args ...interface{}) {
 	e.eb.Publish(topic, args...)
+}
+
+// 注销topic事件的fn监听
+func (e *eventBusWrapper) Unsubscribe(topic string, fn interface{}) {
+	e.eb.Unsubscribe(topic, fn)
 }
