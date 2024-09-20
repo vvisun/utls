de67bc4e4441f75b8137a8787f12b0b47a107b6e
vvisun <374708234@qq.com>
2024-09-20 20:48:53 +0800

    Update api.go

--- errutil/api.go
+++ errutil/api.go
@@ -2,27 +2,42 @@ package errutil
 
 import (
 	"errors"
+	"fmt"
 	"runtime"
 )
 
 var err_map map[int32]error = make(map[int32]error)
 
 func init() {
-	AddErrTable(err_table)
+	for code, str := range err_table {
+		err_table[code] = str
+		err_map[code] = errors.New(str)
+	}
 }
 
 func AddErrTable(tbl map[int32]string) {
 	for code, str := range tbl {
+		if code < 1000 {
+			fmt.Printf("[error]错误码必须大于1000: %d - %s", code, str)
+		}
 		err_table[code] = str
 		err_map[code] = errors.New(str)
 	}
 }
 
 func Error(code int32) error {
+	if _, ok := err_map[code]; !ok {
+		fmt.Printf("[error]没有配置错误表: %d", code)
+		return errors.New("未知错误(没有配置错误表)")
+	}
 	return err_map[code]
 }
 
 func ErrorString(code int32) string {
+	if _, ok := err_table[code]; !ok {
+		fmt.Printf("[error]没有配置错误表: %d", code)
+		return "未知错误(没有配置错误表)"
+	}
 	return err_table[code]
 }
 
